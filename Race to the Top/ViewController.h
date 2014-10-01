0210619ad5699b0b4a21fc708f8063b1e6a78939
Robin van 't Slot <slotrobin@hotmail.com>
2014-10-01 14:25:35 +0200

    Initial Commit

---
+++ Race to the Top/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Race to the Top
+//
+//  Created by Robin van 't Slot on 01-10-14.
+//  Copyright (c) 2014 BrickInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Race to the Top/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Race to the Top
+//
+//  Created by Robin van 't Slot on 01-10-14.
+//  Copyright (c) 2014 BrickInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
